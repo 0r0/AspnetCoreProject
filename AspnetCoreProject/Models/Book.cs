7708620231a05e25db28c8dc7274f673391b2ec4
mehdi <0r0.developer@gmail.com>
2021-11-25 03:13:21 +0400

    add fields to Book Model

--- AspnetCoreProject/Models/Book.cs
+++ AspnetCoreProject/Models/Book.cs
@@ -7,6 +7,11 @@ namespace AspnetCoreProject.Models
 {
     public class Book
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Email { get; set; }
+        public string BookCateory { get; set; }
+        public string Description { get; set; }
     }
 }
