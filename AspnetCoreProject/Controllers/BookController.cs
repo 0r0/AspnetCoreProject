4511f2455074bae8ece5732faea2d10a57157670
mehdi <0r0.developer@gmail.com>
2021-11-24 22:16:56 +0400

    Add project files.

---
+++ AspnetCoreProject/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowBooks()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspnetCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspnetCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
