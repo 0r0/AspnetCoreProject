54bd6d07b4150fed82a26ec3cfbd3491c09853bd
mehdi <0r0.developer@gmail.com>
2021-11-25 17:11:01 +0400

    create showBook method controller that redirect to bookDetail m controller ,get id and create book object and send to view for showing in page

--- AspnetCoreProject/Controllers/BookController.cs
+++ AspnetCoreProject/Controllers/BookController.cs
@@ -26,7 +26,18 @@ namespace AspnetCoreProject.Controllers
             return View();
         }
 
+        public IActionResult BookDetail([FromRoute] int id)
+        {
+            Book o = new Book();
+            o.Id = id;
+            o.Title = string.Format("Book title {0}",id);
+            o.Author = string.Format("Author {0}",id);
+            o.Email = string.Format("dotnet{0}@email.com", id);
+            o.BookCategory = "Programming";
+            o.Description = string.Format("Description for title {0}",id);
 
+            return View("detail",o);
+        }
         
 
         public IActionResult ShowDetail()
@@ -36,11 +47,16 @@ namespace AspnetCoreProject.Controllers
             o.Title = "Essential Asp.Net Core 5";
             o.Author = "Mr .Net";
             o.Email = "DotNet@gmail.com";
-            o.BookCateory = "programming";
+            o.BookCategory = "programming";
             o.Description = "this book  is designed for .Net Developers";
 
             return View(o);
 
+        }
+
+        public IActionResult ShowBook(int id)
+        {
+            return RedirectToAction("BookDetail",new { id=id});
         }
         public IActionResult ViewComponentDemo()
         {
