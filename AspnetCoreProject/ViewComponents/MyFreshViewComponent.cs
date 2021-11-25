eb43389fb1d7b4a8cf7f2e6477f1c39706909a4e
mehdi <0r0.developer@gmail.com>
2021-11-25 11:43:36 +0400

    create test Myfresh view component that do add and substract and name it MyFresh

---
+++ AspnetCoreProject/ViewComponents/MyFreshViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace AspnetCoreProject.ViewComponents
+{   [ViewComponent(Name ="MyFresh")]
+    public class MyFreshViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int a, int b,string op)
+        {
+            int result = 0;
+            if (op == "add")
+            {
+                result = a + b;
+            }
+            else if (op == "subtract")
+            {
+                result = a - b;
+            }
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.op = op;
+            ViewBag.result = result;
+            return View();
+        }
+    }
+}
