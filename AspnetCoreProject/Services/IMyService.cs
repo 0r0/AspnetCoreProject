ddfccfdda62c754e0fa9c5d24600ebaf52f1c84b
mehdi <0r0.developer@gmail.com>
2021-11-25 17:16:33 +0400

    create a service interface that calculate a and b parameter

---
+++ AspnetCoreProject/Services/IMyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    public interface IMyService
+    {
+        int Calculate(int a, int b);
+    }
+}
