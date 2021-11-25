3849b11905ac4ccc14192948c44006f11fffab85
mehdi <0r0.developer@gmail.com>
2021-11-26 00:41:10 +0400

    manage 404 Error paths

--- AspnetCoreProject/Startup.cs
+++ AspnetCoreProject/Startup.cs
@@ -41,6 +41,15 @@ namespace AspnetCoreProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Home";
+                    await next();
+                }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -54,6 +63,7 @@ namespace AspnetCoreProject
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+      
         }
     }
 }
