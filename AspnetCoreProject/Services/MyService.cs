ce9eb75fa9940bb61ebf975e268057a5c8818cdb
mehdi <0r0.developer@gmail.com>
2021-11-25 18:33:39 +0400

    create a service

---
+++ AspnetCoreProject/Services/MyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    public class MyService : IMyService
+    {
+        public int Calculate(int a, int b)
+        {
+            return a * b + b * 10;
+        }
+    }
+}
