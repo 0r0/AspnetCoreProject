d8a4106240e078a7bbfa7be7d2deea3e2238d6eb
mehdi <0r0.developer@gmail.com>
2021-11-26 10:31:51 +0400

    product model definition

---
+++ AspnetCoreProject/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public int Quantity { get; set; }
+
+
+
+    }
+}
