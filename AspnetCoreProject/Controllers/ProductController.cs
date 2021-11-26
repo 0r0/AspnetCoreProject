ce58ce8c2c5a3238a4e6660e9dce916597453ce9
mehdi <0r0.developer@gmail.com>
2021-11-26 20:40:01 +0400

    add constructor to ProductController for dependency injection and create ShowProduct method that show dummy product in View

--- AspnetCoreProject/Controllers/ProductController.cs
+++ AspnetCoreProject/Controllers/ProductController.cs
@@ -3,30 +3,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AspnetCoreProject.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
 
-namespace AspnetCoreProject.Controllers
+using AspnetCoreProject.Models;
+
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+namespace Section3.Controllers
 {
     public class ProductController : Controller
     {
         private IWebHostEnvironment _environment;
+        public ProductController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Create([FromForm]Product model)
+        public IActionResult Create([FromForm] Product model)
         {
             if (ModelState.IsValid)
+            {
                 return View("Details", model);
+            }
             return View();
         }
         public IActionResult CreateProduct()
@@ -53,8 +60,11 @@ namespace AspnetCoreProject.Controllers
             }
             return View();
         }
+        public IActionResult ShowProducts()
+        {
+      
+            return View();
+        }
+        
     }
-   
-    
-    
 }
