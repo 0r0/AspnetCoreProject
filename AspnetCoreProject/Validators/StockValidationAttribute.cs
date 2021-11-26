7d26f6cbde1d6c02b8966eda2b71763e7e5babb6
mehdi <0r0.developer@gmail.com>
2021-11-26 15:56:52 +0400

    define body of validation for Quantity field in product

--- AspnetCoreProject/Validators/StockValidationAttribute.cs
+++ AspnetCoreProject/Validators/StockValidationAttribute.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using AspnetCoreProject.Models;
+
 namespace AspnetCoreProject.Validators
 {
     public class StockValidationAttribute: ValidationAttribute
     {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Product product =(Product) validationContext.ObjectInstance;
+            if (product.Quantity < 5)
+                return new ValidationResult("Product stock has minimum 5");
+            return ValidationResult.Success;
+        }
     }
 }
