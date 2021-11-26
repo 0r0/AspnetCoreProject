6ef3fe58c4db4f1e950f2d3ac130fa24091d0959
mehdi <0r0.developer@gmail.com>
2021-11-26 10:51:41 +0400

    add Create method that get Data from form with post method As a Product Model

--- AspnetCoreProject/Controllers/ProductController.cs
+++ AspnetCoreProject/Controllers/ProductController.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using AspnetCoreProject.Models;
 namespace AspnetCoreProject.Controllers
 {
     public class ProductController : Controller
@@ -14,6 +14,11 @@ namespace AspnetCoreProject.Controllers
         }
 
         public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product Model)
         {
             return View();
         }
