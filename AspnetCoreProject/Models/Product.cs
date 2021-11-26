86468d2a05ee8f8c9b200de5bde02778bc8d95ea
mehdi <0r0.developer@gmail.com>
2021-11-26 11:20:51 +0400

    change display name of model fields by using component annotation

--- AspnetCoreProject/Models/Product.cs
+++ AspnetCoreProject/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,13 @@ namespace AspnetCoreProject.Models
 {
     public class Product
     {
+        [Display(Name="Product Id")]
         public int Id { get; set; }
+        [Display(Name="Product Name")]
         public string Name { get; set; }
-
+        [Display(Name="Product Price")]
         public float Price { get; set; }
-
+        [Display(Name="Product Quantity")]
         public int Quantity { get; set; }
 
 
