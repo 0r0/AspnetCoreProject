4d59ae5a3ae77beb8d74170f3b6867fc3fcefa0d
mehdi <0r0.developer@gmail.com>
2021-11-29 23:32:08 +0400

    change name and  add validation to fields

--- AspnetCoreProject/Models/Employee.cs
+++ AspnetCoreProject/Models/Employee.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using AspnetCoreProject.Validators;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspnetCoreProject.Models
 {
     public partial class Employee
     {
+        [Display(Name ="Employee Id")]
+        [Required]
         public int Id { get; set; }
+        [Display(Name =" name")]
+        [Required]
         public string FirstName { get; set; }
+        [Display(Name ="family")]
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        
+        [Required]
         public int Age { get; set; }
+        [Display(Name ="Creational Time")]
         public DateTime Created { get; set; }
 
         public virtual EmployeePicture EmployeePicture { get; set; }
