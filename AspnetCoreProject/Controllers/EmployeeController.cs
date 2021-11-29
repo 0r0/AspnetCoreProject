f3aa3803e9462e5f5d7fea6998b1e32b0ff62d9d
mehdi <0r0.developer@gmail.com>
2021-11-29 11:52:44 +0400

    define Controller class for Employee Model and define Index method that show all Employee table data

---
+++ AspnetCoreProject/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreProject.Models;
+namespace AspnetCoreProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeProjectContext _context;
+
+        public EmployeeController(EmployeeProjectContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Employees);
+        }
+      
+    }
+}
