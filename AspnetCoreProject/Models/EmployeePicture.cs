937ff539da00407ea31d351270c31f963109014b
mehdi <0r0.developer@gmail.com>
2021-11-30 23:53:36 +0400

    make image info field required

--- AspnetCoreProject/Models/EmployeePicture.cs
+++ AspnetCoreProject/Models/EmployeePicture.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AspnetCoreProject.Models
 {
     public partial class EmployeePicture
     {
         public int EmpId { get; set; }
+        [Required(ErrorMessage ="it is required field")]
         public string ImageInfo { get; set; }
         public string ImageType { get; set; }
         public byte[] ImageData { get; set; }
