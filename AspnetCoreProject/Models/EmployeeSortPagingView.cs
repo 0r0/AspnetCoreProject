edbc40c62cb4aef7fa4ea011ce4b83922a6b79cd
mehdi <0r0.developer@gmail.com>
2021-12-01 11:49:32 +0400

    create a model for sort employees object in pagination

---
+++ AspnetCoreProject/Models/EmployeeSortPagingView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class EmployeeSortPagingView
+    {
+        public List<Employee> Employees { get; set; }
+        public int CurrentIndexPage { get; set; }
+        public int PageCount { get; set; }
+        public string SortField { get; set; }
+        public string CurrentSortField { get; set; }
+        public string CurrentSortOrder { get; set; }
+    }
+}
