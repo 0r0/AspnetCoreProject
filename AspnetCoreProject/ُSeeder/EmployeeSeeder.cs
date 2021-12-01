4f7ee5fd94672628e1822ce6d8ed42c17c371fef
mehdi <0r0.developer@gmail.com>
2021-12-02 01:52:08 +0400

    make a seeder for Employee

---
+++ AspnetCoreProject/ُSeeder/EmployeeSeeder.cs
@@ -0,0 +1,32 @@
+﻿using AspnetCoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject._ُSeeder
+{
+    public class EmployeeSeeder
+    {
+        private EmployeeProjectContext ContextForSeeding;
+        public EmployeeSeeder(EmployeeProjectContext employeeProjectContext)
+        {
+            ContextForSeeding = employeeProjectContext;
+        }
+        public void Seed(int NumberOfSeed)
+        {
+            for(int i=0;i<NumberOfSeed;i++)
+            {
+                Employee employee = new Employee();
+                employee.FirstName = Faker.Name.First();
+                employee.LastName = Faker.Name.Last();
+                employee.Age =  new Random().Next(14, 100);
+                employee.Email = Faker.Internet.Email();
+                employee.Created = DateTime.Now;
+                ContextForSeeding.Employees.Add(employee);
+
+            }
+            ContextForSeeding.SaveChanges();
+        }
+    }
+}
