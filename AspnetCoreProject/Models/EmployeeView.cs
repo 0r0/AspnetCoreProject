f98195e92c5156abc66b1f24e5080d188843b74b
mehdi <0r0.developer@gmail.com>
2021-12-01 11:38:50 +0400

    create employee view for pagination

---
+++ AspnetCoreProject/Models/EmployeeView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class EmployeeView
+    {
+        public List<Employee> Employees { get; set; }
+        public int CurrentPageIndex {get;set;}
+        public int PageCount { get; set; }
+
+
+
+
+        
+    }
+}
