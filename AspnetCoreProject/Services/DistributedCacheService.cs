74d885577a1cb072a481aad40365e70061e8b9ba
mehdi <0r0.developer@gmail.com>
2021-12-04 12:09:22 +0400

    implement cache interface service for get and set and remove cache

---
+++ AspnetCoreProject/Services/DistributedCacheService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    public class DistributedCacheService : IDistributedCacheService
+    {
+        public IDistributedCache _cache;
+        public DistributedCacheService(IDistributedCache _cache)
+        {
+            this._cache = _cache;
+        }
+        public T GetCache<T>(string key) where T : class
+        {
+            try {
+                byte[] values = _cache.Get(key);
+                if (values == null)
+                    return null;
+                T result = JsonSerializer.Deserialize<T>(values);
+                return result;
+            }
+            catch(Exception){ }
+                return null;
+        }
+        public void SetCache<T>(string key, T values)
+        {
+            try
+            {
+                var bytes = JsonSerializer.SerializeToUtf8Bytes(values, typeof(T));
+                DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions() {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(1),
+                    SlidingExpiration=TimeSpan.FromSeconds(30)
+                };
+                _cache.Set(key, bytes, cacheOptions);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void RemoveCache(string key)
+        {
+            try
+            {
+                _cache.Remove(key);
+            }
+            catch (Exception) { }
+        }
+
+       
+    }
+}
