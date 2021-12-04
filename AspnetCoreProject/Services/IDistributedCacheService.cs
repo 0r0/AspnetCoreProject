d629acb038efa13f4bdacd5afccfbdcafcd12cd5
mehdi <0r0.developer@gmail.com>
2021-12-04 12:08:51 +0400

    define cache interface service

---
+++ AspnetCoreProject/Services/IDistributedCacheService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+   public interface IDistributedCacheService
+    {
+        void SetCache<T>(String key, T values);
+        T GetCache<T>(string key) where T : class;
+        void RemoveCache(string key);
+    }
+}
