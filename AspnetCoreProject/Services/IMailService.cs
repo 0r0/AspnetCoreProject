c9311d701cece87ccf606f1e46c3e16346db9c2e
mehdi <0r0.developer@gmail.com>
2021-12-06 01:06:28 +0400

    IMailService interface for using in mail service

---
+++ AspnetCoreProject/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using AspnetCoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    public interface IMailService
+    {
+        Task sendEmailAsync(MailRequest mailRequest);
+    }
+}
