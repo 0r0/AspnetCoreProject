dca9da8719c50d534c120337e68fb36e37731e96
mehdi <0r0.developer@gmail.com>
2021-12-06 01:05:11 +0400

    define AccountController with initialization of Register methods and construct all Authentication and authorization manager

--- AspnetCoreProject/Controllers/AccountController.cs
+++ AspnetCoreProject/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspnetCoreProject.Models;
+using AspnetCoreProject.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,54 @@ namespace AspnetCoreProject.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private IMailService _mailService;
+        public AccountController(UserManager<AppUser> userManager,
+            IMailService mailService,SignInManager<AppUser> signInManager
+            ,RoleManager<IdentityRole> roleManager)
+        
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] UserView model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.UserName,
+                    FullName = model.FullName,
+                    Email = model.Email
+
+                };
+                //check identity
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                    //add role
+                    //IdentityResult result2 = await _userManager.AddToRoleAsync(user, "Member");
+                    //if (result2.Succeeded)
+                    //{
+                    //    return RedirectToAction("Index");
+                    //}
+                }
+            }
+            return View(model);
         }
     }
 }
