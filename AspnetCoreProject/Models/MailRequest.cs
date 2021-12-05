ac845b0be7db6ab21799e2243bf819f745268c5f
mehdi <0r0.developer@gmail.com>
2021-12-06 01:05:36 +0400

    define mailRequest model

---
+++ AspnetCoreProject/Models/MailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class MailRequest
+    {
+        public string ToEmail { set; get; }
+        public string Subject { set; get; }
+        public string Body { set; get; }
+    }
+}
