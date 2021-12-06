e1f887dca5df4889345b5c57de40bb7a90a06d1c
mehdi <0r0.developer@gmail.com>
2021-12-06 11:20:42 +0400

    create loginview model that use for login

---
+++ AspnetCoreProject/Models/LoginView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class LoginView
+    {
+        [Required]
+        [UIHint("username")]
+        public string UserName { get; set; }
+        [Required]
+        [UIHint("Password")]
+        public string Password { get; set; }
+    }
+}
