13afa857657fe968efb4e7288d3eeb635020fddb
mehdi <0r0.developer@gmail.com>
2021-12-06 16:02:05 +0400

    define a controller  for admin role

---
+++ AspnetCoreProject/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using AspnetCoreProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     
+    }
+}
