ecd4dfff998db177a280822fbdc3312b820a3c5b
mehdi <0r0.developer@gmail.com>
2021-12-06 11:21:10 +0400

    define mailservice skeleton

---
+++ AspnetCoreProject/Services/MailService.cs
@@ -0,0 +1,16 @@
+﻿using AspnetCoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    public class MailService : IMailService
+    {
+        public Task sendEmailAsync(MailRequest mailRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
