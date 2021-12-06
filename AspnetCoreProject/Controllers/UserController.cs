649f53cb3f7ffe274705351d7f41991c8d2c9461
mehdi <0r0.developer@gmail.com>
2021-12-06 15:47:35 +0400

    make a controller for user

---
+++ AspnetCoreProject/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.User = User.Identity.Name;
+
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Anonymous()
+        {
+            return View(); 
+        }
+            }
+}
