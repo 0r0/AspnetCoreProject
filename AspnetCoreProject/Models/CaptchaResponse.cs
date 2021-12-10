7e86103b7d655d7a0023ec149eae3529e411038a
mehdi <0r0.developer@gmail.com>
2021-12-10 20:24:18 +0400

    defination of  captcha response

---
+++ AspnetCoreProject/Models/CaptchaResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class CaptchaResponse
+    {
+        public bool success { get; set; }
+        public DateTime challenge_ts { get; set; }
+        public string hostname { get; set; }
+        
+    }
+}
