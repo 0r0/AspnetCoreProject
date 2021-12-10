46956b945dcb72c15a8b9105130938bb47267454
mehdi <0r0.developer@gmail.com>
2021-12-10 20:24:53 +0400

    define interface of Captcha Service

---
+++ AspnetCoreProject/Services/IRecaptchaService.cs
@@ -0,0 +1,14 @@
+﻿using AspnetCoreProject.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Services
+{
+    interface IRecaptchaService
+    {
+        ReCaptchaSettings Configs { get; }
+        bool ValidateRecaptcha(string token);
+    }
+}
