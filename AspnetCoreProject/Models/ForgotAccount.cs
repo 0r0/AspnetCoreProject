9d87d9721597cf30a4fe223db335a831a2e030cc
mehdi <0r0.developer@gmail.com>
2021-12-11 11:02:04 +0400

    forgot account email

---
+++ AspnetCoreProject/Models/ForgotAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreProject.Models
+{
+    public class ForgotAccount
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
